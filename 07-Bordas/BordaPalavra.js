06b561ae8716bf8a9b2bb08262072a13f5cc6463
Deivid Veloso <veloso.deivid@gmail.com>
2018-11-12 08:19:58 -0200

    bordas

---
+++ 07-Bordas/BordaPalavra.js
@@ -0,0 +1,10 @@
+//const texto ="dia diagnostico diatonico diafragma media wikipedia bom_dia melodia radial";
+
+const texto2 =
+  "dia diatônico diafragma, média wikipédia bom-dia melodia radial";
+
+//console.log(texto.match(/\bdia\w*/gi));
+//console.log(texto.match(/\w+dia\b/gi));
+//console.log(texto.match(/\w+dia\w+/gi));
+//console.log(texto.match(/\bdia\b/gi));
+console.log(texto2.match(/\bdia\b/gi));
